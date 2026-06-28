8530dc038acc16408c7b35d7c68e455cb2de043e
Chad Moravec <chad.m.moravec@gmail.com>
2026-06-27 20:47:20 -0600

    Optimize images for mobile; allow AI agents in robots.txt

--- tina/__generated__/client.ts
+++ tina/__generated__/client.ts
@@ -1,5 +1,5 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: '', queries,  });
+export const client = createClient({ cacheDir: '/Users/chadmoravec/Desktop/Projects/Clients/pivotal-tides-WORKING-SOURCE/tina/__generated__/.cache/1782519100454', url: 'https://content.tinajs.io/1.6/content/2b3148a9-1bb0-4b09-8e8c-51b55263e90a/github/blog-preview', token: '041b4a5f10ef0f6bd6b15e873adae82e62230f73', queries,  });
 export default client;
   
--- tina/__generated__/types.ts
+++ tina/__generated__/types.ts
@@ -464,7 +464,7 @@ export const ExperimentalGetTinaClient = () =>
   getSdk(
     generateRequester(
       createClient({
-        url: "http://localhost:4001/graphql",
+        url: "https://content.tinajs.io/1.6/content/2b3148a9-1bb0-4b09-8e8c-51b55263e90a/github/blog-preview",
         queries,
       })
     )
